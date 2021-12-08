b257b71a63d28cb15c61dbe4f5623070af884cfc
Aqib <aqibrashidpandit@gmail.com>
2021-12-08 21:13:35 +0530

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! # Reporter
+//!
+//! `reporter` will help you sending a piece of information or message to different destinations e.g. email inbox, slack channel etc
+
 pub mod reporter;
 
 pub use reporter::*;
