6c90407bdeb1f01893ae7790604ee8405aac6508
Aqib <aqibrashidpandit@gmail.com>
2021-12-08 21:15:32 +0530

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # Reporter
 //!
-//! `reporter` will help you sending a piece of information or message to different destinations e.g. email inbox, slack channel etc
+//! `reporter` will help you in sending a piece of information or message to different destinations e.g. email inbox, slack channel etc
 
 pub mod reporter;
 
